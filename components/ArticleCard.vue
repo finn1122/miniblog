dd3f82c7960ad6d58b4885af3c9c30f5c26e44f2
finn1122 <mauroivaning@gmail.com>
2022-10-21 15:20:49 -0500

    setup netlify

---
+++ components/ArticleCard.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="article-card">
+         <NuxtLink :to="slug"><h3>{{ title }}</h3></NuxtLink>
+        <div class="author">
+        <p>Por {{ author }}</p>
+        <small>Fecha de publicacion: {{ date }}</small>
+        <figure>
+            <img :src="cover" alt="cover">
+        </figure>
+        <p>{{ description }}</p>
+        <div class="actions">
+            <NuxtLink class="btn" :to="slug">Ir al post</NuxtLink>
+            
+        </div>
+        </div>
+    </div>
+   
+</template>
+
+<script>
+export default {
+    name: 'ArticleCard',
+    props: {
+        slug: {
+            type: String,
+            default: '',
+
+        },
+        title: {
+            type: String,
+            default: '',
+
+        },
+        author: {
+            type: String,
+            default: '',
+
+        },
+        date: {
+            type: String,
+            default: '',
+
+        },
+        cover: {
+            type: String,
+            default: '',
+
+        },
+        description: {
+            type: String,
+            default: '',
+
+        },
+    }
+}
+</script>
+
+<style lang="scss">
+.article-card {
+    @apply border border-red-700;
+}
+</style>
